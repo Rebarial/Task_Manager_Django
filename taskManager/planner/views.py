4f1ee4a2d461fe3d2902a762a1b8b98d8232345d
Reber <reberalko@gmail.com>
2024-04-03 12:35:12 +1000

    Switching the calendar

--- taskManager/planner/views.py
+++ taskManager/planner/views.py
@@ -5,17 +5,36 @@ from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required, permission_required
 from .models import Task
 from django.contrib.auth.models import User
+from time import strptime
 # Create your views here.
 
 
 @login_required
 def index(request):
     tasks = Task.objects.all().filter(user = request.user).order_by('data','time')
-    print(request.GET.get('param1', None))
-    print(request.user)
-    print(tasks)
-    monday = datetime.datetime.now() - datetime.timedelta(datetime.datetime.now().weekday()+7)
-    weekday = []
+    weekday_name = ['Понедельник', 'Вторник','Среда','Четверг','Пятница','Суббота','Воскресенье']
+    if ('date' in request.GET):
+        date = request.GET['date']
+        if date == '':
+            date = datetime.datetime.now()
+        else:
+            date = datetime.datetime.strptime(date,'%Y-%m-%d')
+        if (request.GET['action'] == 'Last'):
+            print(date.weekday())
+            monday = (date -
+                      datetime.timedelta(7-date.weekday()))
+        if (request.GET['action'] == 'Next'):
+            monday = (date +
+                      datetime.timedelta(7-date.weekday()))
+        if (request.GET['action'] == 'Select'):
+            monday = (date -
+                      datetime.timedelta(date.weekday()))
+    else:
+        monday = datetime.datetime.now() - datetime.timedelta(datetime.datetime.now().weekday())
+        print(monday)
+    weekdata = []
     for i in range(7):
-        weekday.append((monday + datetime.timedelta(i)).date())
-    return render(request, 'planner/index.html', {'weekday': weekday, 'tasks': tasks})
+        weekdata.append((monday + datetime.timedelta(i)).date())
+
+    weekday = list(zip(weekday_name, weekdata))
+    return render(request, 'planner/index.html', {'weekday': weekday, 'tasks': tasks, 'username': request.user})
