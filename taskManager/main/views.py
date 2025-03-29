4ae09d531d79c6af743e021682dd5ca0106a652f
Rebarial <reberalko@gmail.com>
2025-03-29 16:41:07 +1000

    little change index page

--- taskManager/main/views.py
+++ taskManager/main/views.py
@@ -1,9 +1,11 @@
 from django.contrib.auth import logout
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.shortcuts import redirect
 # Create your views here.
 
 def index(request):
+    #return redirect('/accounts/login')
     return render(request, 'main/index.html')
 
 def logout_view(request):
